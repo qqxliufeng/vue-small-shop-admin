bfccbdec0258fbaacd58be87276be6b0511a2061
lf <lf@192.168.0.105>
2019-10-16 18:25:38 +0800

    修改了搜索列表中的bug

--- src/pages/goods/SearchGoods.vue
+++ src/pages/goods/SearchGoods.vue
@@ -53,7 +53,7 @@ export default {
   },
   computed: {
     isCanShare () {
-      return Boolean(this.$root.state.canShareTicket)
+      return this.$root.state.canShareTicket === '1'
     }
   },
   methods: {
